2fc5a7888fdab58e36f91e1f267d1567ff8706ec
Guillermo-Latumahina <g.latumahina@icloud.com>
2022-11-04 12:38:13 +0100

    added route functionality

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -2,19 +2,25 @@ import React from 'react';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
+import SNavbar from "./components/navbar/navbar";
+import Shop from "./pages/shop/Shop";
+import Cart from "./pages/shop/Cart";
+import Orders from "./pages/shop/Orders";
+import AddProduct from "./pages/admin/AddProduct";
+import AdminProducts from "./pages/admin/AdminProducts";
 
 function App() {
     return (
         <div className="App">
+            <SNavbar/>
             <BrowserRouter>
                 <Routes>
-                    <Route path="/"/>
-                    <Route path="/products"/>
-                    <Route path="/cart"/>
-                    <Route path="/orders"/>
-                    <Route path="/admin/add-product"/>
-                    <Route path="/admin/products"/>
+                    <Route path="/" element={<Shop/>}/>
+                    <Route path="/products" element={<Shop/>}/>
+                    <Route path="/cart" element={<Cart/>}/>
+                    <Route path="/orders" element={<Orders/>}/>
+                    <Route path="/admin/add-product" element={<AddProduct/>}/>
+                    <Route path="/admin/products" element={<AdminProducts/>}/>
                 </Routes>
 
             </BrowserRouter>
@@ -22,4 +28,4 @@ function App() {
     );
 }
 
-export default App;
+export default App
