017ba4a30d6b165796fbca37c2075c8908da16f3
Guillermo-Latumahina <g.latumahina@icloud.com>
2022-11-04 12:34:35 +0100

    added navbar

---
+++ front-end/src/components/navbar/navbar.tsx
@@ -0,0 +1,26 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+export default function SNavbar() {
+    return (
+        <Navbar bg="light" expand="lg">
+            <Container>
+                <Navbar.Brand href="/">Shop Side Project</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="/products">Products</Nav.Link>
+                        <Nav.Link href="/cart">Cart</Nav.Link>
+                        <Nav.Link href="/orders">Orders</Nav.Link>
+                        <NavDropdown title="Admin" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="/admin/add-product">Add Product</NavDropdown.Item>
+                            <NavDropdown.Item href="/admin/Products">Admin Products</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
