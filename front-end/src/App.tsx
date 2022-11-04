47f2ec565dd97606561ac8fa647958f395b1d20f
Guillermo-Latumahina <g.latumahina@icloud.com>
2022-11-04 12:34:03 +0100

    added routes

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/"/>
+                    <Route path="/products"/>
+                    <Route path="/cart"/>
+                    <Route path="/orders"/>
+                    <Route path="/admin/add-product"/>
+                    <Route path="/admin/products"/>
+                </Routes>
+
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
